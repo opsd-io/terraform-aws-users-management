4b4ae183ef36c195c1bf15a716b0d29947f51ed9
Plasny <git.plasny.uq95y@slmail.me>
2022-12-01 16:24:25 +0100

    added support for list of keys

--- modules/users/main.tf
+++ modules/users/main.tf
@@ -1,12 +1,25 @@
+locals {
+  key_defaults = {
+    key_encoding = "SSH"
+    key_status   = "Active"
+  }
+  final_ssh_keys = {
+    for k, v in var.ssh_keys :
+    k => merge(local.key_defaults, v)
+  }
+}
+
 resource "aws_iam_user" "main" {
-  name = var.iam_user_name
-  path = var.iam_user_path
-  tags = var.iam_user_tags
+  name = var.user_name
+  path = var.user_path
+  tags = var.user_tags
 }
 
 resource "aws_iam_user_ssh_key" "main" {
+  count = length(local.final_ssh_keys)
+
   username   = aws_iam_user.main.name
-  encoding   = var.iam_user_ssh_key_encoding
-  public_key = var.iam_user_ssh_public_key
-  status     = var.iam_user_ssh_key_status
+  public_key = local.final_ssh_keys[count.index].key
+  encoding   = local.final_ssh_keys[count.index].key_encoding
+  status     = local.final_ssh_keys[count.index].key_status
 }
--- modules/users/variables.tf
+++ modules/users/variables.tf
@@ -1,20 +1,20 @@
-variable "iam_user_name" {
+variable "user_name" {
   description = "AWS IAM user name"
   type        = string
 
   validation {
-    condition     = can(regex("^[a-zA-Z\\+\\=\\,\\.\\@\\-\\_0-9]{1,64}$", var.iam_user_name))
+    condition     = can(regex("^[a-zA-Z\\+\\=\\,\\.\\@\\-\\_0-9]{1,64}$", var.user_name))
     error_message = "User names can be a combination of up to 64 letters, digits, and these characters: plus (+), equal (=), comma (,), period (.), at sign (@), underscore (_), and hyphen (-)."
   }
 }
 
-variable "iam_user_path" {
+variable "user_path" {
   description = "Path in which to create the AWS IAM user."
   type        = string
   default     = "/"
 }
 
-variable "iam_user_tags" {
+variable "user_tags" {
   description = "Tags assign to this resource."
   type        = map(string)
   default     = {}
@@ -22,25 +22,14 @@ variable "iam_user_tags" {
   # tag key must be a minimum of 1 and a maximum of 128 Unicode characters in UTF-8.
   # tag value must be a minimum of 0 and a maximum of 256 Unicode characters in UTF-8.
   # validation {
-  #   condition     = can(regex("^[a-zA-Z\\+\\=\\,\\.\\@\\-\\_0-9]{1,128}$", var.iam_user_tags))
+  #   condition     = can(regex("^[a-zA-Z\\+\\=\\,\\.\\@\\-\\_0-9]{1,128}$", var.user_tags))
   #   error_message = "The allowed characters are letters, numbers, spaces representable in UTF-8, and the following characters: underscore (_), dot(.), colon(:), slash (/), equal (=), plus (+), hyphen (-) and at sign (@)."
   # }
 }
 
-variable "iam_user_ssh_key_encoding" {
-  description = "Specifies the public key encoding format to use in the response."
-  type        = string
-  default     = "SSH" # Avaliable options: SSH and PEM
-}
-
-variable "iam_user_ssh_public_key" {
-  description = "Unique identifier for the SSH public key."
-  type        = string
-  default     = ""
-}
-
-variable "iam_user_ssh_key_status" {
-  description = "Status of the SSH/PEM public key"
-  type        = string
-  default     = "Active" # Avaliable options: Active and Inactive
+variable "ssh_keys" {
+  description = "List of objects containing: key encoding format (SSH/PEM), ssh key itself, status of the key (Active/Inactive)"
+  type = list(object({
+    key = string
+  }))
 }
