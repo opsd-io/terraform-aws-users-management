ff37a4165c2a2e538dd094f6d2529048f2587564
Plasny <git.plasny.uq95y@slmail.me>
2022-12-02 11:54:16 +0100

    adding ouputs

---
+++ modules/users/outputs.tf
@@ -0,0 +1,25 @@
+output "ssh_keys" {
+  description = "List of ssh_key objects, each containing public key, its owner and its encoding"
+  value = local.ssh_keys 
+}
+
+output "user_arns" {
+  description = "List of arns"
+  value = [
+    for user in aws_iam_user.main : user.arn
+  ]
+}
+
+output "user_ids" {
+  description = "List of user IDs"
+  value = [
+    for user in aws_iam_user.main : user.id
+  ]
+}
+
+output "user_unique_ids" {
+  description = "List of user unique IDs"
+  value = [
+    for user in aws_iam_user.main : user.unique_id
+  ]
+}
