899c54cf13459f1278bcf2a442dda14d961593d2
Plasny <git.plasny.uq95y@slmail.me>
2022-12-09 12:40:00 +0100

    terraform fmt...

--- modules/groups/main.tf
+++ modules/groups/main.tf
@@ -11,7 +11,7 @@ resource "aws_iam_group" "main" {
 }
 
 resource "aws_iam_group_policy_attachment" "main" {
-  for_each   = { for k, v in local.roles : k => v }
+  for_each = { for k, v in local.roles : k => v }
 
   group      = aws_iam_group.main.name
   policy_arn = each.value
