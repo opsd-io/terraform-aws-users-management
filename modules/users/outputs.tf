2db4e792bb32494ddf4183ae11afafbce64935f5
Plasny <git.plasny.uq95y@slmail.me>
2022-12-14 10:32:53 +0100

    changing outputs from objects to single values

--- modules/users/outputs.tf
+++ modules/users/outputs.tf
@@ -1,19 +1,19 @@
 output "user_arn" {
   description = "IAM user arn"
-  value       = { (aws_iam_user.main.name) = aws_iam_user.main.arn }
+  value       = aws_iam_user.main.arn
 }
 
 output "user_id" {
   description = "IAM user id"
-  value       = { (aws_iam_user.main.name) = aws_iam_user.main.id }
+  value       = aws_iam_user.main.id
 }
 
 output "user_unique_id" {
   description = "IAM user unique id"
-  value       = { (aws_iam_user.main.name) = aws_iam_user.main.unique_id }
+  value       = aws_iam_user.main.unique_id
 }
 
 output "user_password" {
   description = "IAM user unique id"
-  value       = { (aws_iam_user.main.name) = aws_iam_user_login_profile.main.password }
+  value       = aws_iam_user_login_profile.main.password
 }
