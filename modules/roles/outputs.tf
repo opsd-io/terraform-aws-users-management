3b6bc7aa2216dc245a8000dcc3808079b9195dae
Plasny <git.plasny.uq95y@slmail.me>
2022-12-14 10:29:47 +0100

    changing outputs from objects to single values

--- modules/roles/outputs.tf
+++ modules/roles/outputs.tf
@@ -1,19 +1,19 @@
 output "role_arn" {
   description = "IAM Role arn"
-  value       = { (aws_iam_role.main.name) = aws_iam_role.main.arn }
+  value       = aws_iam_role.main.arn
 }
 
 output "role_id" {
   description = "IAM Role id"
-  value       = { (aws_iam_role.main.name) = aws_iam_role.main.id }
+  value       = aws_iam_role.main.id
 }
 
 output "role_unique_id" {
   description = "IAM Role unique id"
-  value       = { (aws_iam_role.main.name) = aws_iam_role.main.unique_id }
+  value       = aws_iam_role.main.unique_id
 }
 
 output "assume_role_policy" {
   description = "Name of the IAM role and the arn of policy that allows assuming the role"
-  value       = { (aws_iam_role.main.name) = aws_iam_policy.assume_role.arn }
+  value       = aws_iam_policy.assume_role.arn
 }
