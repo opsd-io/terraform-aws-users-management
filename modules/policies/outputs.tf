df90f57995d0de153d18e367ad150612c7539ab7
Plasny <git.plasny.uq95y@slmail.me>
2022-12-14 10:22:19 +0100

    changing outputs from objects to single values

--- modules/policies/outputs.tf
+++ modules/policies/outputs.tf
@@ -1,14 +1,14 @@
 output "policy_arn" {
   description = "IAM Policy arn"
-  value       = { (aws_iam_policy.main.name) = aws_iam_policy.main.arn }
+  value       = aws_iam_policy.main.arn
 }
 
 output "policy_id" {
   description = "IAM Policy id"
-  value       = { (aws_iam_policy.main.name) = aws_iam_policy.main.id }
+  value       = aws_iam_policy.main.id
 }
 
 output "policy_policy_id" {
   description = "IAM Policy policy id"
-  value       = { (aws_iam_policy.main.name) = aws_iam_policy.main.policy_id }
+  value       = aws_iam_policy.main.policy_id
 }
